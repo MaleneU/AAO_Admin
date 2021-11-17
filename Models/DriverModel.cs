0b1dc7a704299879edbc300f4a95d81296699b58
André Hørslev Dijkhuizen <andr440n@edu.ucl.dk>
2021-11-17 11:22:20 +0100

    Driver Model

---
+++ Models/DriverModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAO_AdminPanel.Models
+{
+    public class DriverModel
+    {
+        public int DriverID { get; set; }
+        public int UserID { get; set; }
+        public sbyte Active { get; set; }
+        public int StartLocationID { get; set; }
+        public int TypeID { get; set; }
+    }
+}
