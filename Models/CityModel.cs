004adee24ad0f210dae45005a594f97763ee3189
miadp88 <54668407+miadp88@users.noreply.github.com>
2021-11-17 13:46:43 +0100

    Test - CityModel + data constraints

---
+++ Models/CityModel.cs
@@ -0,0 +1,27 @@
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAO_AdminPanel.Models
+{
+    public class CityModel
+    {
+        [Key]
+        public int CityID { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public  string City { get; set; }
+
+        [Column(TypeName = "varchar(45)")]
+        public string ZipCode { get; set; }
+
+        public int CountryID { get; set; }
+
+        //public List<CountryModel> CountryModel  { get; set; }
+    }
+}
+
