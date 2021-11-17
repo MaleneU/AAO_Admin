689ea351e893e4808a49670ba9fbaaafb0c9f62b
André Hørslev Dijkhuizen <andr440n@edu.ucl.dk>
2021-11-17 11:44:46 +0100

    Start Location Model

---
+++ Models/StartLocationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAO_AdminPanel.Models
+{
+    public class StartLocationModel
+    {
+        public int StartLocationID { get; set; }
+        public string Location { get; set; }
+    }
+}
