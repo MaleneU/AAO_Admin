02da27090f4fd51f7eb1ee85e9474e9eee955b4a
André Hørslev Dijkhuizen <andr440n@edu.ucl.dk>
2021-11-17 11:51:03 +0100

    Traffic Model

---
+++ Models/TrafficModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAO_AdminPanel.Models
+{
+    public class TrafficModel
+    {
+        public int TrafficID { get; set; }
+        public int StartCountryID { get; set; }
+        public int StopCountryID { get; set; }
+        public int TypeID { get; set; }
+    }
+}
