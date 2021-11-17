586af1fa9fdf2ff615704a5aac6eb5c71be53ed9
André Hørslev Dijkhuizen <andr440n@edu.ucl.dk>
2021-11-17 11:18:51 +0100

    Driver License Model - Unsure of datatypes

---
+++ Models/DriverLicenseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AAO_AdminPanel.Models
+{
+    public class DriverLicenseModel
+    {
+        public int LicenseID { get; set; }
+        public int DriverID { get; set; }
+        public DateTime ExpirationDate { get; set; } // Forkert datatype?
+        public string Image { get; set; } // Forkert datatype?
+        public sbyte Active { get; set; } // Usikker på datatype
+    }
+}
