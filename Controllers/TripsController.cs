2be48523704e73956801e28bb3d44345c2744411
KatharinaAppel <katharinaappel@gmail.com>
2021-11-24 10:50:50 +0100

    Trips CRUD

---
+++ Controllers/TripsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AAO_AdminPanel.Data;
+using AAO_AdminPanel.Models;
+
+namespace AAO_AdminPanel.Controllers
+{
+    public class TripsController : Controller
+    {
+        private readonly MySQLDbContext _context;
+
+        public TripsController(MySQLDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trips
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.Trip.ToListAsync());
+        }
+
+        // GET: Trips/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trip
+                .FirstOrDefaultAsync(m => m.TripID == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trips/Create
+        public IActionResult Create()
+        {
+            PopulateDepartmentsDropDownList(); 
+            //ViewData["Department"] = new SelectList(_context.Department, "DepartmentID", "DepartmentID");
+            return View();
+        }
+
+        // POST: Trips/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TripID,StartDateAndTime,StopDate,Duration,DepartmentID,Description,Urgent")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trip);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trip.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trips/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TripID,StartDateAndTime,StopDate,Duration,Description,Urgent")] Trip trip)
+        {
+            if (id != trip.TripID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trip);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TripExists(trip.TripID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trip
+                .FirstOrDefaultAsync(m => m.TripID == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // POST: Trips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trip = await _context.Trip.FindAsync(id);
+            _context.Trip.Remove(trip);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TripExists(int id)
+        {
+            return _context.Trip.Any(e => e.TripID == id);
+        }
+    }
+}
