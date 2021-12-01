53eb52691430c5150ea12ddd76a3d8ad97d658f0
Mikkel Desktop <msjo36725@edu.ucl.dk>
2021-12-01 10:22:01 +0100

    Request Controller og views.

---
+++ Controllers/RequestsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AAO_AdminPanel.Data;
+using AAO_AdminPanel.Models;
+
+namespace AAO_AdminPanel.Controllers
+{
+    public class RequestsController : Controller
+    {
+        private readonly MySQLDbContext _context;
+
+        public RequestsController(MySQLDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Requests
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Request.ToListAsync());
+        }
+
+        // GET: Requests/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Request
+                .FirstOrDefaultAsync(m => m.RequestID == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // GET: Requests/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RequestID")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(request);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Request.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+            return View(request);
+        }
+
+        // POST: Requests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RequestID")] Request request)
+        {
+            if (id != request.RequestID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(request);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequestExists(request.RequestID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Request
+                .FirstOrDefaultAsync(m => m.RequestID == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // POST: Requests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var request = await _context.Request.FindAsync(id);
+            _context.Request.Remove(request);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequestExists(int id)
+        {
+            return _context.Request.Any(e => e.RequestID == id);
+        }
+    }
+}
---
+++ ViewModels/TripViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AAO_AdminPanel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AAO_AdminPanel.ViewModels
+{
+    public class TripViewModel
+    {
+        //public List<Trip> Trips { get; set; }
+        //public List<User> Users { get; set; }
+       // public List<Traffic> Traffics { get; set; }
+       // public List<Department> Departments { get; set; }
+
+        public IEnumerable<TripModel> Trips { get; set; }
+    }
+}
