f72c43995e20bd51858a8b641ddb7a6b6e5c4649
Gert1337 <Katharinaappel@gmail.com>
2021-12-02 12:48:28 +0100

    Index opdated on trips

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -13,19 +13,39 @@ namespace AAO_AdminPanel.Models
 
         [Key]
         public int TripID { get; set; }
+        
+        [Display(Name = "Startdato og tid")]
         public DateTime StartDateAndTime { get; set; }
+
+        [Display(Name = "Stop dato")]
         public DateTime StopDate { get; set; }
+
+        [Display(Name = "Varighed")]
         public int Duration { get; set; }
         public int UserID { get; set; }
+
+        [Display(Name = "Chauffør")]
         public User User { get; set; }
         [Column(TypeName = "varchar(255)")]
+
+        [Display(Name = "Beskrivelse")]
         public string Description { get; set; }
         public int TrafficID { get; set; }
+
+        [Display(Name = "Traffik")]
         public Traffic Traffic { get; set; }
         public int DepartmentID { get; set; }
+
+        [Display(Name = "Afdeling")]
         public Department Department { get; set; }
+
+
         public int StartLocationID { get; set; }
+
+        [Display(Name = "Start Lokation")]
         public StartLocation Startlocation { get; set; }
+
+        [Display(Name = "Haste tur")]
         public bool Urgent { get; set; }
         public List<Request> Requests { get; set; }
 
