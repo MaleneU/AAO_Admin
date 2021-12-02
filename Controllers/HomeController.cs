7ff78e2b69e6ad69f59936d21fafc66f06f6557d
Malene Ulm <msul36749@edu.ucl.dk>
2021-12-02 10:10:42 +0100

    Log in authorization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace AAO_AdminPanel.Controllers
-{   // [Authorize] // Uncomment for log in demo
+{   [Authorize] // Log in needed
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
