516da75b04c0f564368ca213c487dd5fac3dce35
André Hørslev Dijkhuizen <andr440n@edu.ucl.dk>
2021-12-02 13:30:53 +0100

    DriverController

---
+++ Controllers/DriversController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AAO_AdminPanel.Data;
+using AAO_AdminPanel.Models;
+
+namespace AAO_AdminPanel.Controllers
+{
+    public class DriversController : Controller
+    {
+        private readonly MySQLDbContext _context;
+
+        public DriversController(MySQLDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Drivers
+        public async Task<IActionResult> Index()
+        {
+            var mySQLDbContext = _context.Driver.Include(d => d.StartLocation).Include(d => d.TrafficType).Include(d => d.User).Include(d => d.DriverLicenses).ThenInclude(d => d.License);
+            return View(await mySQLDbContext.ToListAsync());
+        }
+
+        // GET: Drivers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Driver
+                .Include(d => d.StartLocation)
+                .Include(d => d.TrafficType)
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.DriverID == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+
+            return View(driver);
+        }
+
+        // GET: Drivers/Create
+        public IActionResult Create()
+        {
+            ViewData["StartLocationID"] = new SelectList(_context.StartLocation, "StartLocationID", "StartLocationID");
+            ViewData["TrafficTypeID"] = new SelectList(_context.Type, "TypeID", "TypeID");
+            ViewData["DriverUsers"] = _context.User.Where(r => r.RoleID == 2);
+            return View();
+        }
+
+        // POST: Drivers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DriverID,UserID,Active,StartLocationID,TrafficTypeID")] Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(driver);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StartLocationID"] = new SelectList(_context.StartLocation, "StartLocationID", "StartLocationID", driver.StartLocationID);
+            ViewData["TrafficTypeID"] = new SelectList(_context.Type, "TypeID", "TypeID", driver.TrafficTypeID);
+            ViewData["UserID"] = new SelectList(_context.User, "UserID", "UserID", driver.UserID);
+            return View(driver);
+        }
+
+        // GET: Drivers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Driver.FindAsync(id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+            ViewData["StartLocationID"] = new SelectList(_context.StartLocation, "StartLocationID", "StartLocationID", driver.StartLocationID);
+            ViewData["TrafficTypeID"] = new SelectList(_context.Type, "TypeID", "TypeID", driver.TrafficTypeID);
+            ViewData["UserID"] = new SelectList(_context.User, "UserID", "UserID", driver.UserID);
+            return View(driver);
+        }
+
+        // POST: Drivers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DriverID,UserID,Active,StartLocationID,TrafficTypeID")] Driver driver)
+        {
+            if (id != driver.DriverID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(driver);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DriverExists(driver.DriverID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StartLocationID"] = new SelectList(_context.StartLocation, "StartLocationID", "StartLocationID", driver.StartLocationID);
+            ViewData["TrafficTypeID"] = new SelectList(_context.Type, "TypeID", "TypeID", driver.TrafficTypeID);
+            ViewData["UserID"] = new SelectList(_context.User, "UserID", "UserID", driver.UserID);
+            return View(driver);
+        }
+
+        // GET: Drivers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Driver
+                .Include(d => d.StartLocation)
+                .Include(d => d.TrafficType)
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.DriverID == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+
+            return View(driver);
+        }
+
+        // POST: Drivers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var driver = await _context.Driver.FindAsync(id);
+            _context.Driver.Remove(driver);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DriverExists(int id)
+        {
+            return _context.Driver.Any(e => e.DriverID == id);
+        }
+    }
+}
---
+++ Migrations/MySQLDb/20211202115755_DriverLicense List.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AAO_AdminPanel.Migrations.MySQLDb
+{
+    public partial class DriverLicenseList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DriverLicense_Driver_DriverID",
+                table: "DriverLicense");
+
+            migrationBuilder.DropColumn(
+                name: "DriveID",
+                table: "DriverLicense");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DriverID",
+                table: "DriverLicense",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DriverLicense_Driver_DriverID",
+                table: "DriverLicense",
+                column: "DriverID",
+                principalTable: "Driver",
+                principalColumn: "DriverID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DriverLicense_Driver_DriverID",
+                table: "DriverLicense");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DriverID",
+                table: "DriverLicense",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DriveID",
+                table: "DriverLicense",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DriverLicense_Driver_DriverID",
+                table: "DriverLicense",
+                column: "DriverID",
+                principalTable: "Driver",
+                principalColumn: "DriverID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
