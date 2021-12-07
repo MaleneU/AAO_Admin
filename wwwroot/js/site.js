7e88b96f876f9ca22b4c1e60e5bdc54277e52621
miadp88 <54668407+miadp88@users.noreply.github.com>
2021-12-07 14:40:27 +0100

    implementing side-navigation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,3 +15,37 @@ $(".rotate").click(function () {
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+$("#leftside-navigation .sub-menu > a").click(function (e) {
+    $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+        e.stopPropagation()
+})
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.left = "0px";
+    document.getElementById("mainContent").style.marginLeft = "260px";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.left = "-240px";
+    document.getElementById("mainContent").style.marginLeft = "60px";
+
+}
